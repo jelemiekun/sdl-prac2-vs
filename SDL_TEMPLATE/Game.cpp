af3942d343323007060d474fdeb5aeccf88d0705
jelemiekun - pc <jeremieeden1946@gmail.com>
2024-11-21 19:12:01 +0800

    Starting point of moving background offset

--- SDL_TEMPLATE/Game.cpp
+++ SDL_TEMPLATE/Game.cpp
@@ -3,8 +3,8 @@
 #include <cmath>
 #include <string>
 
-Game::Game() : isRunning(false), gWindow(nullptr), gRenderer(nullptr), texturePercolation(nullptr),
-		percolation(nullptr), percStats(nullptr), gFont(nullptr) {
+Game::Game() : isRunning(false), gWindow(nullptr), gRenderer(nullptr), textureBackground(nullptr),
+		gFont(nullptr) {
 
 }
 
@@ -54,10 +54,9 @@ void Game::init(const char* title, const int& rXPos, const int& rYPos, const int
 	}
 
 	int widthHeightTexture = 700;
-	texturePercolation = SDL_CreateTexture(gRenderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_TARGET, 700, 700);
+	textureBackground = SDL_CreateTexture(gRenderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_TARGET, 700, 700);
 
 
-	percStats = new PercolationStats;
 
 	isRunning = true;
 }
@@ -69,7 +68,6 @@ void Game::input() {
 		} else if (gEvent.type == SDL_KEYDOWN) {
 			switch (gEvent.key.keysym.sym) {
 			case SDLK_SPACE:
-				percolation->openRandomSite();
 				break;
 			default:
 				break;
@@ -79,97 +77,14 @@ void Game::input() {
 }
 
 void Game::update() {
-	if (percolation == nullptr) {
-		percolation = new Percolation(numberOfRowColumn, 700);
-	} else {
-		for (int i = 0; i < 100; i++) {
-			percolation->openRandomSite();
-		}
-		
-		if (percolation->percolates()) {
-			percStats->addOpenSite(percolation->getNumberOfOpenSites());
-			percStats->compute();
 
-			delete percolation;
-			percolation = nullptr;
-		}
-	}
 }
 
 void Game::render() {
 	SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0);
 	SDL_RenderClear(gRenderer);
 
-	if (percolation != nullptr) {
-		int wText;
-		int hText;
 
-		SDL_QueryTexture(texturePercolation, NULL, NULL, &wText, &hText);
-		SDL_Rect textureRect = { 50, 100, wText, hText };
-		SDL_RenderCopy(gRenderer, texturePercolation, nullptr, &textureRect);
-
-		SDL_SetRenderTarget(gRenderer, texturePercolation);
-		percolation->render(gRenderer);
-
-		SDL_SetRenderTarget(gRenderer, nullptr);
-	}
-
-	{
-
-		auto render = [](SDL_Renderer*& gRenderer, TTF_Font* font, std::string message, SDL_Rect dstRect) -> void {
-			SDL_Texture* textDisplay = nullptr;
-			// Free the existing texture if it exists
-			if (textDisplay != nullptr) {
-				SDL_DestroyTexture(textDisplay);
-				textDisplay = nullptr;
-			}
-
-			SDL_Color white = { 255, 255, 255, 255 };
-
-			// Create the text surface
-			SDL_Surface* textSurface = TTF_RenderText_Solid(font, message.c_str(), white);
-			if (textSurface == nullptr) {
-				std::cout << "Unable to render text surface: " << TTF_GetError() << '\n';
-				return; // Return early if the surface couldn't be created
-			}
-
-			// Create a texture from the surface
-			textDisplay = SDL_CreateTextureFromSurface(gRenderer, textSurface);
-			if (textDisplay == nullptr) {
-				std::cout << "Failed to create texture from rendered text: " << SDL_GetError() << '\n';
-			}
-
-			// Free the surface after creating the texture
-			SDL_FreeSurface(textSurface);
-
-			// Only render if texture creation was successful
-			if (textDisplay != nullptr) {
-				SDL_RenderCopy(gRenderer, textDisplay, nullptr, &dstRect);
-			}
-
-			SDL_DestroyTexture(textDisplay);
-			textDisplay = nullptr;
-			};
-
-		// Call render for each line of text
-		SDL_Rect rectTimes = { 50, 10, 220, 40 };
-		render(gRenderer, gFont, "Mean: " + std::to_string(percStats->getMean()), rectTimes);
-
-		rectTimes = { 50, 50, 270, 40 };
-		render(gRenderer, gFont, "Standard Deviation: " + std::to_string(percStats->getStdDev()), rectTimes);
-
-		rectTimes = { 350, 10, 270, 40 };
-		render(gRenderer, gFont, "Confidence Low: " + std::to_string(percStats->getConfidenceLo()), rectTimes);
-
-		rectTimes = { 350, 50, 270, 40 };
-		render(gRenderer, gFont, "Confidence High: " + std::to_string(percStats->getConfidenceHi()), rectTimes);
-
-		rectTimes = { 660, 10, 90, 40 };
-		render(gRenderer, gFont, "Count: " + std::to_string(percStats->getTimes()), rectTimes);
-
-		rectTimes = { 650, 50, 100, 40 };
-		render(gRenderer, gFont, "# of Squares: " + std::to_string(numberOfRowColumn * numberOfRowColumn), rectTimes);
-	}
 
 	SDL_RenderPresent(gRenderer);
 }
